521b9ee8b703cfc86120531c1e6951776070f0bd
maxjdev <maxjramosdev@gmail.com>
2023-12-04 17:08:27 -0300

    implementando lógica na classe Dev

--- abstraindo_bootcamp_usando_poo_java/src/br/com/desafio/dominio/Dev.java
+++ abstraindo_bootcamp_usando_poo_java/src/br/com/desafio/dominio/Dev.java
@@ -1,7 +1,7 @@
 package br.com.desafio.dominio;
-
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,11 +9,26 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp() {}
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosConcluidos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
 
-    public void progredir() {}
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo !");
+        }
+    }
 
-    public void calcularTotalXP() {}
+    public Double calcularTotalXP() {
+        return this.conteudosConcluidos
+                .stream()
+                .mapToDouble(Conteudo::calcularXp)
+                .sum();
+    }
 
     public String getNome() {
         return nome;
